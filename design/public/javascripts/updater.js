0eb107fd1c10f910eb2346e9296d72b85c8a46af
Thijs van der Vossen <thijs@fngtps.com>
2010-06-04 14:53:27 +0200

    Add vote submission and results loading. Still ugly, but functional.

--- design/public/javascripts/updater.js
+++ design/public/javascripts/updater.js
@@ -1,11 +1,12 @@
 if (typeof RT == "undefined") RT = {};
 
-RT.delay = 60;
+RT.delay = 3;
 
 RT.Updater = Class.create({
   initialize: function() {
     this.form = $('vote').down('form');
     this.progressBar = $('progress').down('div');
+    this.results = $('results');
     this.request = false;
     this.resetTimer();
     new PeriodicalExecuter(this.updateTimer.bind(this), 1);
@@ -20,9 +21,13 @@ RT.Updater = Class.create({
     }.bind(this), 50);
   },
   
+  mostRecentMinuteId: function() {
+    return this.results.down('div.m').id.substring(2);
+  },
+  
   updateTimer: function(executer) {
     var left = (RT.delay + (this.then - new Date().getTime()) / 1000);
-    console.log(left);
+    // console.log(left);
     if (left > 0) {
       var width = left / RT.delay * 100;
       if (width < 0) {
@@ -32,13 +37,21 @@ RT.Updater = Class.create({
     } else if (!this.request) {
       this.progressBar.setStyle({width: '0'});
       $(document.body).addClassName('loading');
-      this.request = this.form.request({
-        onComplete: function() {
-          setTimeout(function() {  // TODO For development
+      var id = this.mostRecentMinuteId();
+      var url = this.form.getAttribute('action') + '?id=' + id;
+      url = url.gsub('?id=', '-')
+      // TODO For development
+      console.log(url);
+      this.request = new Ajax.Request(url, {
+        parameters: this.form.serialize(true),
+        onSuccess: function(response) {
+          // setTimeout(function() {  // TODO For development
+            console.log('hi');
+            this.results.insert({top: response.responseText});
             this.resetTimer();
             this.request = false;
             $(document.body).removeClassName('loading');
-          }.bind(this), 2500);
+          // }.bind(this), 500);
         }.bind(this)
       });
     }
